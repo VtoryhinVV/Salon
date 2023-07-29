336fa84f09a1ef466e46112e4467189e66d51984
unknown <vlad.vtorygin@gmail.com>
2023-07-29 19:03:52 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import ContactuUs from "./pages/ContactUs";
 import Products from "./pages/Products";
@@ -7,15 +7,17 @@ import { Layout } from "./components/Layout/Layout";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="/about_us" element={<p>About</p>} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/contact" element={<ContactuUs />} />
-        <Route path="*" element={<HomePage />} />
-      </Route>
-    </Routes>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/Salone/"}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/about_us" element={<p>About</p>} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<ContactuUs />} />
+          <Route path="*" element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
