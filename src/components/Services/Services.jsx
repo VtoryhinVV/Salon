52234b106c4687b0467ae6d6a171f8c68fb0aeec
Vad1mVV <107924193+Vad1mVV@users.noreply.github.com>
2023-08-08 19:11:36 +0200

    Desktop version

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,119 @@
+import { useState } from "react";
+import { CSSTransition } from 'react-transition-group';
+
+import { Container } from "../shared/Container"
+import { ServicesSection,
+    ServicesTitle, 
+    ServicesContent, 
+    ServicesList, 
+    ServicesListItem, 
+    ServicesListVisibleContent,
+    ServicesListHiddenContent, 
+    ServicesListItemTitle, 
+    ServicesSvg,
+    ServicesLink
+} from "./Services.styled"
+
+import ServicesPhoto from "/img/Services.jpg";
+import svgImgs from '/symbol-defs.svg';
+import "./style.css"
+
+export const Services = () => {
+    const [open, setOpen] = useState("first");
+    const log = (e) => {
+        setOpen(e.target.id);
+    };
+
+    return <ServicesSection>
+        <Container>
+            <ServicesTitle>Services</ServicesTitle>
+            <ServicesContent>
+                <ServicesList>
+
+                    <ServicesListItem id="first" onClick={log}>
+                        <ServicesListVisibleContent id="first" onClick={log}>
+                            <ServicesListItemTitle id="first" onClick={log}>Dermal filler / facial sculpting</ServicesListItemTitle>
+                            <ServicesSvg id="first" onClick={log}>
+                                <use href={`${svgImgs}#icon-arrow`}></use>
+                            </ServicesSvg>
+                        </ServicesListVisibleContent>
+
+                        
+                            <CSSTransition id="first" in={open === "first"} timeout={200} classNames="my-node">
+                                <ServicesListHiddenContent id="first" open={open === "first"}>
+                                    <p>Facial sculpting filler treatment can enhance the patient’s bone structure in areas such as cheeks, chin     and jawline. The Filler treatment can help both men and women to create more contoured facial features to   obtain a more masculine or feminine look, respectively.</p>
+                                    <ServicesLink to="/products">READ MORE</ServicesLink>
+                                </ServicesListHiddenContent>
+                            </CSSTransition>
+                    </ServicesListItem>
+
+                    <ServicesListItem id="second" onClick={log}>
+                        <ServicesListVisibleContent id="second" onClick={log}>
+                            <ServicesListItemTitle id="second" onClick={log}>Lip Filler</ServicesListItemTitle>
+                            <ServicesSvg id="second" onClick={log}>
+                                <use href={`${svgImgs}#icon-arrow`}></use>
+                            </ServicesSvg>
+                        </ServicesListVisibleContent>
+
+                        <CSSTransition id="second" in={open === "second"} timeout={200} classNames="my-node">
+                            <ServicesListHiddenContent id="second" open={open === "second"} transitionName="example" transitionAppear={true} transitionAppearTimeout={500} transitionEnter={false} transitionLeave={false}>
+                                <p>Facial sculpting filler treatment can enhance the patient’s bone structure in areas such as cheeks, chin and jawline. The Filler treatment can help both men and women to create more contoured facial features to obtain a more masculine or feminine look, respectively.</p>
+                                <ServicesLink to="/products">READ MORE</ServicesLink>
+                            </ServicesListHiddenContent>
+                        </CSSTransition>
+                    </ServicesListItem>
+
+                    <ServicesListItem id="third" onClick={log}>
+                        <ServicesListVisibleContent id="third" onClick={log}>
+                            <ServicesListItemTitle id="third" onClick={log}>Injectables for men</ServicesListItemTitle>
+                            <ServicesSvg id="third" onClick={log}>
+                                <use href={`${svgImgs}#icon-arrow`}></use>
+                            </ServicesSvg>
+                        </ServicesListVisibleContent>
+
+                        <CSSTransition id="third" in={open === "third"} timeout={200} classNames="my-node">
+                            <ServicesListHiddenContent id="third" open={open === "third"}>
+                                <p>Facial sculpting filler treatment can enhance the patient’s bone structure in areas such as cheeks, chin and jawline. The Filler treatment can help both men and women to create more contoured facial features to obtain a more masculine or feminine look, respectively.</p>
+                                <ServicesLink to="/products">READ MORE</ServicesLink>
+                            </ServicesListHiddenContent>
+                        </CSSTransition>
+                    </ServicesListItem>
+
+                    <ServicesListItem id="fourth" onClick={log}>
+                        <ServicesListVisibleContent id="fourth" onClick={log}>
+                            <ServicesListItemTitle id="fourth" onClick={log}>Anti Wrinkle & Wrinkle preventative</ServicesListItemTitle>
+                            <ServicesSvg id="fourth" onClick={log}>
+                                <use href={`${svgImgs}#icon-arrow`}></use>
+                            </ServicesSvg>
+                        </ServicesListVisibleContent>
+
+                        <CSSTransition id="fourth" in={open === "fourth"} timeout={200} classNames="my-node">
+                            <ServicesListHiddenContent id="fourth" open={open === "fourth"}>
+                                <p>Facial sculpting filler treatment can enhance the patient’s bone structure in areas such as cheeks, chin and jawline. The Filler treatment can help both men and women to create more contoured facial features to obtain a more masculine or feminine look, respectively.</p>
+                                <ServicesLink to="/products">READ MORE</ServicesLink>
+                            </ServicesListHiddenContent>
+                        </CSSTransition>    
+                    </ServicesListItem>
+
+                    <ServicesListItem id="fifth" onClick={log}>
+                        <ServicesListVisibleContent id="fifth" onClick={log}>
+                            <ServicesListItemTitle id="fifth" onClick={log}>Medical</ServicesListItemTitle>
+                            <ServicesSvg id="fifth" onClick={log}>
+                                <use href={`${svgImgs}#icon-arrow`}></use>
+                            </ServicesSvg>
+                        </ServicesListVisibleContent>
+
+                        <CSSTransition id="fifth" in={open === "fifth"} timeout={200} classNames="my-node">
+                            <ServicesListHiddenContent id="fifth" open={open === "fifth"}>
+                                <p>Facial sculpting filler treatment can enhance the patient’s bone structure in areas such as cheeks, chin and jawline. The Filler treatment can help both men and women to create more contoured facial features to obtain a more masculine or feminine look, respectively.</p>
+                                <ServicesLink to="/products">READ MORE</ServicesLink>
+                            </ServicesListHiddenContent>
+                        </CSSTransition>    
+                    </ServicesListItem>
+                    
+                </ServicesList>
+                <img src={ServicesPhoto} alt="" height={792} />
+            </ServicesContent>
+        </Container>
+    </ServicesSection>
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import '../App.css';
 import { Hero } from '../components/Hero/Hero';
 import { OurStory } from '../components/OurStory/OurStory';
+import { Services } from '../components/Services/Services';
 import { WhyUs } from '../components/WhyUs/WhyUs';
 
 export function HomePage() {
@@ -9,6 +10,7 @@ export function HomePage() {
       <Hero />
       <OurStory />
       <WhyUs />
+      <Services />
     </>
   );
 }
