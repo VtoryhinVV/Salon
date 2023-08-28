c19e1a7b815c737c5202d5bb15ee440f4df0c837
Vlad Vtoryhin <vlad.vtorygin@gmail.com>
2023-08-28 21:33:26 +0300

    Upd routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
-import ContactuUs from './pages/ContactUs';
-import Products from './pages/Products';
+
 import { Layout } from './components/Layout/Layout';
+import { ServicesChoose } from './pages/ServicesChoose';
 
 function App() {
   return (
@@ -11,9 +11,18 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="/about_us" element={<p>About</p>} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/contact" element={<ContactuUs />} />
+          <Route path="about_us" element={<p>About</p>} />
+          <Route path="services" element={<p>services</p>} >
+            {/* <Route path="dermal_filler" element={<p>Dermal filler</p>} />
+            <Route path="lip_filler" element={<p>Lip Filler</p>} />
+            <Route path="injectables_for_men" element={<p>Injectables for men</p>} />
+            <Route path="medical" element={<p>Medical</p>} />
+            <Route path="anti_wrinkle" element={<p>Anti Wrinkle</p>} /> */}
+          </Route>
+          <Route path="/services/:servicesChoose" element={<ServicesChoose />} />
+          <Route path="pricing" element={<p>pricing</p>} />
+          <Route path="vouchers" element={<p>vouchers</p>} />
+          <Route path="contact_us" element={<p>contact_us</p>} />
           <Route path="*" element={<HomePage />} />
         </Route>
       </Routes>
