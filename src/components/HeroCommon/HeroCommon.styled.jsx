5875298d930c4b59df643764abbbcddc2780e4e5
Vad1mVV <107924193+Vad1mVV@users.noreply.github.com>
2023-08-30 19:33:35 +0200

    1

---
+++ src/components/HeroCommon/HeroCommon.styled.jsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled';
+import bgHero from '/img/bg.jpg';
+
+export const HeroSection = styled.section`
+    background-image: url(${bgHero});
+    padding: 120px 0;
+
+    h1, p, button {
+        margin: 0;
+    }
+    & > div {
+        text-align: center;
+        align-items: center;
+
+        display: flex;
+        flex-direction: column;
+        gap: 24px;
+    }
+`
+
+export const HeroTitle = styled.h1 `
+    font-family: Prata;
+    font-size: 48px;
+    letter-spacing: 0.96px;
+`
+
+export const HeroText = styled.h1 `
+    font-weight: 700;
+`
+
+export const HeroButton = styled.button `
+    width: 225px;
+
+    border: 1px solid var(--brown);
+    background: var(--white, #fff);
+    padding: 12px 24px;
+
+    color: var(--brown);
+    text-transform: uppercase;
+
+    transition: all 0.3s ease;
+
+    &:hover,
+    &:focus {
+        border-color: var(--focus);
+        color: var(--focus);
+    }
+`
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,9 @@
+import { HeroCommon } from "../components/HeroCommon/HeroCommon"
+
+export const AboutUs = () => {
+    return <>
+        <HeroCommon title='Discover Our story' 
+                    text='Short story about us & meet the team' 
+                    buttonText='Book appoinment' />
+    </>
+}
