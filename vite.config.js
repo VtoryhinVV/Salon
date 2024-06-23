95797f2320d0ea7b9ec23ca3993c3bfb526b3849
Vlad Vtoryhin <vlad.vtorygin@gmail.com>
2024-06-23 13:41:04 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   const config = {
-    base: '/Salon',
+    base: '/',
     plugins: [react()],
     assetsInclude: ['**/*.MOV'],
   };
